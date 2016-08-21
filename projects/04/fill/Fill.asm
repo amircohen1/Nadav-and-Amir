a35fa58de1bd19bbf95afd522b5c79ea5040c506
amircohen1 <amir.cohen3@mail.huji.ac.il>
2016-08-21 19:28:48 +0300

    project04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,11 +1,75 @@
+
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+
+//blacken the screen
+
+
+
+@curpixel
+M=0
+D=M
+
+(LOOP)
+    @KBD
+    D=M
+    @BLACKSCREEN
+    D;JNE
+    @WHITESCREEN
+    D;JEQ
+
+(BLACKSCREEN)
+    @curpixel
+    D=M // save the cur position in screen
+
+    @SCREEN
+    A=A+D
+    M=-1 // blacken the pixel
+
+    @curpixel
+    D=M
+    @8191
+    D=D-A
+
+    // check if the pointer reached the end of the screen
+    @LOOP
+    D;JEQ
+    //else
+    @curpixel
+    M=M+1
+    @LOOP
+    0;JMP
+
+(WHITESCREEN)
+    //check if the cur pixel is the first pixel
+    @curpixel
+    D=M
+
+    //whiten the pixel
+    @SCREEN
+    A=A+D
+    M=0
+
+    @LOOP //check if it's the first pix
+    D;JEQ
+
+    // else - decrement the pix by 1 and go back to loop
+    @curpixel
+    M= M-1
+
+    @LOOP
+    0;JMP
+
+
+
+
+
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,38 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+//instantiate R2 to 0
+@R2
+M=0
+//check if one of the given vals is 0
+    @R0
+    D=M
+    @END
+    D;JEQ
+    @R1
+    D=M
+    @END
+    D;JEQ
+(LOOP)
+    //put the value of R[0] in D
+    @R0
+    D=M
+
+    //increase the val of R[2] by R[0]
+    @R2
+    M=M+D
+
+    //Decrease the "counter"
+    @R1
+    M=M-1
+    D=M
+
+    //check the loop condition
+    @LOOP
+    D;JNE
+(END)
+    @END
+    0;JMP //infinite loop
+
+
